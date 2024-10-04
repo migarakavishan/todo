b8220defdb1f2dcfa1c57ebaf79f3e33de20df7a
migarakavishan <migarakavishan43@gmail.com>
2024-10-05 00:36:39 +0530

    commit

--- backend/services/user.services.js
+++ backend/services/user.services.js
@@ -6,7 +6,7 @@ class UserService {
             const createUser = new UserModel({email,password});
             return await createUser.save();
         }catch(err) {
-            throw err;
+            
         }
     }
 }
