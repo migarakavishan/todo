bde638c6b921c77a689c6dc71dd2affe16a662a2
migarakavishan <migarakavishan43@gmail.com>
2024-10-12 22:12:28 +0530

    "Added new route for user login"

--- backend/routers/user.router.js
+++ backend/routers/user.router.js
@@ -2,5 +2,6 @@ const router = require('express').Router();
 const UserController = require("../controllers/user.controller");
 
 router.post('/registration',UserController.register);
+router.post('/login',UserController.register);
 
 module.exports  = router; 
