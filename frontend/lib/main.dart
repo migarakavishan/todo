d6773810793aed9952ab306c559373136673f36e
migarakavishan <migarakavishan43@gmail.com>
2024-10-17 20:53:48 +0530

    "Added token-based authentication and conditional routing to main.dart"

--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -1,24 +1,34 @@
+// ignore_for_file: prefer_typing_uninitialized_variables
+
 import 'package:flutter/material.dart';
 import 'package:frontend/auth_screen/login.dart';
+import 'package:frontend/dashboard/dashboard.dart';
+import 'package:jwt_decoder/jwt_decoder.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
-void main() {
-  runApp(const MyApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  SharedPreferences prefs = await SharedPreferences.getInstance();
+  runApp(MyApp(token: prefs.getString('token')));
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  final token;
+
+  const MyApp({@required this.token, super.key});
 
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'TODO',
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const LoginScreen(),
-    );
+        title: 'TODO',
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+          useMaterial3: true,
+        ),
+        home: (JwtDecoder.isExpired(token) == false)
+            ? Dashboard(token: token)
+            : const LoginScreen());
   }
 }
