70a5a10e4e45edce169cad467c58aeb1fe9f8fe7
migarakavishan <migarakavishan43@gmail.com>
2024-10-18 23:46:44 +0530

    "Added TodoModel import to backend/index.js"

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,7 @@
 const app = require('./app');
 const db = require('./config/db')
 const UserModel = require('./model/user.model')
+const TodoModel = require('./model/todo.model')
 
 const port = 3000;
 
---
+++ backend/model/todo.model.js
@@ -0,0 +1,24 @@
+const db = require('../config/db');
+const mongoose = require('mongoose');
+const UserModel = require("../model/user.model")
+const {Schema} = require('mongoose');
+
+const todoSchema = new Schema({
+    userId:{
+        type: Schema.Types.ObjectId,
+        ref:UserModel.modelName
+    },
+    title: {
+        type: String,
+        required : true,
+        
+    },
+    desc: {
+        type: String,
+        required: true
+    }
+});
+
+const TodoModel = db.model('todo',todoSchema);
+
+module.exports = TodoModel;
