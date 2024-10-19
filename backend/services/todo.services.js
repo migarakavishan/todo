4df4ef063627a1af2c970aa006b9077300951feb
migarakavishan <migarakavishan43@gmail.com>
2024-10-19 21:33:01 +0530

    service done

---
+++ backend/services/todo.services.js
@@ -0,0 +1,10 @@
+const TodoModel = require("../model/todo.model");
+
+class TodoServices {
+    static async createTodo(userId, title, desc){
+        const createTodo = new TodoModel({userId,title,desc});
+        return await createTodo.save();
+    }
+}
+
+module.exports = TodoServices;
