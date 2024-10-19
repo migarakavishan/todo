53cdd8a7c92df76bc1fe9fc22f265f3abc80328a
migarakavishan <migarakavishan43@gmail.com>
2024-10-19 23:36:43 +0530

    "Added TodoRouter to app.js, created TodoController and TodoRouter files with createTodo functionality"

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const body_parser = require('body-parser');
 const userRouter = require('./routers/user.router');
+const TodoRouter = require('./routers/todo.router');
 
  
 const app = express();
@@ -8,5 +9,6 @@ const app = express();
 app.use(body_parser.json());
 
 app.use('/',userRouter);
+app.use('/',TodoRouter);
 
 module.exports = app;
